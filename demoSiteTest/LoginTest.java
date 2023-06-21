103820a0654175a05975f22abee51bcb91dac3b1
Rafiulshan19 <18-38321-2@student.aiub.edu>
2023-06-21 12:03:04 +0600

    Testing demo websites

---
+++ demoSiteTest/LoginTest.java
@@ -0,0 +1,28 @@
+package demoSiteTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
+		
+//		driver.get("https://www.saucedemo.com/");
+		driver.navigate().to("https://www.saucedemo.com/");
+		
+		/*Maximize the window*/
+		driver.manage().window().maximize();
+		
+		Thread.sleep(3000);
+		
+		driver.close();
+		
+		driver.quit();
+	}
+
+}
